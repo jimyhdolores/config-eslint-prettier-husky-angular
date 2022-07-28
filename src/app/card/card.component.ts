d8a8d5941a87e4b44fcb25b77e042f17ce23ddfb
jimmy <jimmyhd_19@hotmail.com>
2022-07-28 00:53:59 -0500

    probando segunda forma

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+	@Input() name?: string;
 	//crear variables
 	message?: string;
 
